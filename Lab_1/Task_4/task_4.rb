b9b4841058d5751188b4a0c7c172731c81b2dcef
Nikita Soroka <nikitasoroka228@gmail.com>
2023-03-24 03:34:36 +0300

    1.26 is done

--- Lab_1/Task_4/task_4.rb
+++ Lab_1/Task_4/task_4.rb
@@ -8,6 +8,19 @@ def inter_between_max(array, interval)
   array.count {|el| el >= interval[0] && el <= interval[1]}
 end
 
+# Дан целочисленный массив. Найти количество элементов между первым и последним минимальным.
+def between_first_and_last_min(array)
+  sorted_array = array.sort
+  # puts "#{sorted_array}"
+  ind_min_first = array.index(sorted_array[0])
+  ind_min_last = array.index(sorted_array[1])
+  if ind_min_first > ind_min_last
+    ind_min_first - ind_min_last - 1
+  else
+    ind_min_last - ind_min_first - 1
+  end
+end
+
 puts "Введите массив: "
 array = gets.chomp.split(' ').map(&:to_i)
 puts "Массив #{array}"
@@ -15,7 +28,7 @@ puts "Массив #{array}"
 puts "Выберете метод:
 2: Найти индекс минимального элемента;
 14: Найти кол-во элементов в интервале;
-26: ;
+26: Найти количество элементов между первым и последним минимальным;
 38: ;
 50: .
 "
@@ -29,6 +42,8 @@ when 14
   interval = gets.chomp.split(' ').map(&:to_i)
   # puts "#{interval}"
   puts "Найти кол-во элементов в интервале: #{inter_between_max(array, interval)}"
+when 26
+  puts "Количество элементов между первым и последним минимальным: #{between_first_and_last_min(array)}"
 else
   puts "Нет такого метода"
 end
