3bd6b1fbd728b6439726abf1e662c98f94a61bfa
Nikita Soroka <nikitasoroka228@gmail.com>
2023-04-07 13:28:48 +0300

    attr_accessors

--- Lab_2/Task_1/student.rb
+++ Lab_2/Task_1/student.rb
@@ -1,80 +1,15 @@
 class Student
+  attr_accessor :surname, :name, :lastname, :id, :phone, :telegram, :email, :git
 
-  def initialize(surname , name , lastname , id: nil, phone: nil, telegram: nil, email: nil, git: nil)
+  def initialize(surname , name , lastname , attribut = {})
     @surname = surname
     @name = name
     @lastname = lastname
-    @id = id
-    @phone = phone
-    @telegram = telegram
-    @email = email
-    @git = git
-  end
-
-  # геттеры
-  def id
-    @id
-  end
-
-  def name
-    @name
-  end
-
-  def surname
-    @surname
-  end
-
-  def lastname
-    @lastname
-  end
-
-  def phone
-    @phone
-  end
-
-  def telegram
-    @telegram
-  end
-
-  def email
-    @email
-  end
-
-  def git
-    @git
-  end
-
-  # сеттеры
-  def id=(id)
-    @id = id
-  end
-
-  def name=(name)
-    @name = name
-  end
-
-  def surname=(surname)
-    @surname = surname
-  end
-
-  def lastname=(lastname)
-    @lastname = lastname
-  end
-
-  def phone=(phone)
-    @phone = phone
-  end
-
-  def telegram=(telegram)
-    @telegram = telegram
-  end
-
-  def email=(email)
-    @email = email
-  end
-
-  def git=(git)
-    @git = git
+    @id = attribut[:id]
+    @phone = attribut[:phone]
+    @telegram = attribut[:telegram]
+    @email = attribut[:email]
+    @git = attribut[:git]
   end
 
   def info
