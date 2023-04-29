aa8509f51083d1f1ae2b8d48020ad09104c9fdef
Niksa7 <nikitasoroka228@gmail.com>
2023-04-29 13:02:05 +0300

    Добавлена валидация остальных полей

--- Lab_2/Task_1/main.rb
+++ Lab_2/Task_1/main.rb
@@ -11,6 +11,11 @@ student1 = Student.new(surname='Сорока',
 )
 student2 = Student.new('Епифанцев', 'Всеволод', 'Александрович', id:'2', git:'https://github.com/Vsevolod1817', telegram:'@SevaGrut', email:'seva.epifantsev@gmail.com')
 # student1.phone= '+78925121826'
-student2.phone="+798237846745"
-
-student2.info
+student2.phone="+79823784674"
+student2.to_s
+#Проверка валидации
+puts Student.valid_phone?('+79823784674')
+puts Student.valid_telegram?('@NekiMan0')
+puts Student.valid_git?('Vsevolod1817')
+puts Student.valid_name?('Артем')
+puts Student.valid_email?('nikitasoroka228@gmail.com')
--- Lab_2/Task_1/student.rb
+++ Lab_2/Task_1/student.rb
@@ -12,16 +12,40 @@ class Student
     @git = attribut[:git]
   end
   #Валидация
+  def self.valid_name?(value)
+    value.nil? || value.match?(/^[A-Za-zА-Яа-я]+$/)
+  end
   def self.valid_phone?(phone)
     phone.nil? || phone.match?(/^((\+7|7|8)+([0-9]){10})$/)
+  end
+  def self.valid_telegram?(value)
+    value.nil? || value.match?(/^@[A-Za-z0-9_-]+$/)
+  end
+  def self.valid_git?(value)
+    value.nil? || value.match?(/\Ahttps:\/\/github\.com\/[a-zA-Z0-9]+\z/)
+  end
+  def self.valid_email?(value)
+    value.nil? || value.match?(/^[A-Za-z0-9]+@(gmail)\.(com)/)
   end
   #Сеттеры
   def phone=(value)
     raise ArgumentError, "Недопустимый формат телефонного номера" unless self.class.valid_phone?(value)
     @phone = value
   end
+  def telegram=(value)
+    raise ArgumentError, "Недопустимый формат" unless self.class.valid_telegram?(value)
+    @telegram = value
+  end
+  def email=(value)
+    raise ArgumentError, "Недопустимый формат" unless self.class.valid_email?(value)
+    @email = value
+  end
+  def git=(value)
+    raise ArgumentError, "Недопустимый формат" unless self.class.valid_git?(value)
+    @git = value
+  end
   #Информация об объекте
-  def info
+  def to_s
     puts "Фамилия: #{surname}\nИмя: #{name}\nОтчество: #{lastname}\nID: #{id}\nТелефон: #{phone}\nТелеграм:#{telegram}\nПочта: #{email}\nGit: #{git}\n"
   end
 
