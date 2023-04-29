ed55002a3338d1bc9539b98ee32aa532eebcc4cd
Niksa7 <nikitasoroka228@gmail.com>
2023-04-29 10:26:33 +0300

    Валидация телефонного номера создана

--- Lab_2/Task_1/main.rb
+++ Lab_2/Task_1/main.rb
@@ -1,5 +1,16 @@
 require_relative 'student'
 
-student1 = Student.new('Сорока', 'Никита', 'Алексеевич')
+student1 = Student.new(surname='Сорока',
+                       name='Никита',
+                       lastname='Алексеевич',
+                       id:'1',
+                       git:'https://github.com/Niksa7',
+                       phone:'+99825121826',
+                       telegram:'@NekiManO',
+                       email:'nikitasoroka228@gmail.com'
+)
+student2 = Student.new('Епифанцев', 'Всеволод', 'Александрович', id:'2', git:'https://github.com/Vsevolod1817', telegram:'@SevaGrut', email:'seva.epifantsev@gmail.com')
+# student1.phone= '+78925121826'
+student2.phone="+798237846745"
 
-student1.info
+student2.info
--- Lab_2/Task_1/student.rb
+++ Lab_2/Task_1/student.rb
@@ -11,7 +11,16 @@ class Student
     @email = attribut[:email]
     @git = attribut[:git]
   end
-
+  #Валидация
+  def self.valid_phone?(phone)
+    phone.nil? || phone.match?(/^((\+7|7|8)+([0-9]){10})$/)
+  end
+  #Сеттеры
+  def phone=(value)
+    raise ArgumentError, "Недопустимый формат телефонного номера" unless self.class.valid_phone?(value)
+    @phone = value
+  end
+  #Информация об объекте
   def info
     puts "Фамилия: #{surname}\nИмя: #{name}\nОтчество: #{lastname}\nID: #{id}\nТелефон: #{phone}\nТелеграм:#{telegram}\nПочта: #{email}\nGit: #{git}\n"
   end
