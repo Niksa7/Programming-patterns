ffb34b4567b6ad627864588910a75376c9ab5aba
Niksa7 <nikitasoroka228@gmail.com>
2023-05-25 11:11:24 +0300

    Update Data_table.rb

--- Lab_2/Task_3/Data_table.rb
+++ Lab_2/Task_3/Data_table.rb
@@ -1,6 +1,14 @@
 class Data_table
+  attr_reader :data
   def initialize(data)
     @data = data
+  end
+
+  # Сеттер для замены массива объектов
+  def set_data(new_data)
+    @data = new_data.map.with_index do |row, i|
+      row.map.with_index { |element, j| element.nil? ? get_element(i, j) : element }
+    end
   end
 
   # Получить элемент таблицы
@@ -17,3 +25,30 @@ class Data_table
     @data[0].size
   end
 end
+
+# Экземпляр класса Data_table
+table = Data_table.new([
+                         ["Имя", "Возраст", "City"],
+                         ["Сева", 25, "Лондон"],
+                         ["Никита", 40, "Краснодар"],
+                         ["Артур", 55, "Париж"]
+                       ])
+
+# Исходная таблица
+puts "Original Table:"
+table.data.each { |row| puts row.join(", ") }
+
+# Новый массив объектов
+new_data = [
+  [nil, nil, nil],
+  [nil, nil, "Токио"],
+  ["Ева", 30, nil],
+  ["Алиса", nil, "Берлин"]
+]
+
+# Заменяем массива
+table.set_data(new_data)
+
+# Новая таблица
+puts "Modified Table:"
+table.data.each { |row| puts row.join(", ") }
